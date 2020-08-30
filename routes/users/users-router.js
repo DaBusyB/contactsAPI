dbf33cf6511734cbe41785e4668a2b395f4fa121
DaBusyB <brandiapetsi@gmail.com>
2020-08-30 01:45:42 -0400

    Add login feature

--- routes/users/users-router.js
+++ routes/users/users-router.js
@@ -32,11 +32,33 @@ router.post('/register', async (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+    const {username, password} = req.body
+    let users;
 
+    function findBy(item) {
+        users = knex('users').where(item).orderBy('id')
+        return users
+    }
+
+    try {
+        findBy({username})
+            .first()
+            .then(user => {
+                if( user && bcrypt.compareSync(password, user.password) ) {
+                    req.session.user = user
+                    res.status(200).json({ message: `Welcome ${user.username}` })
+                } else {
+                    res.status(401).json({message: 'Invalid credentials'})
+                }
+            })
+    } catch(err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
 })
 
-router.get('/logout', (req, res) => {
+// router.get('/logout', (req, res) => {
 
-})
+// })
 
 module.exports = router
